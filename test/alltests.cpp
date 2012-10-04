de0d92aa87b6991639c901771faf20b882b0da15
Nick <nick@nick-Inspiron-1750.(none)>
2012-10-04 01:41:12 -0400

    changes

--- test/alltests.cpp
+++
@@ -1,9 +0,0 @@
-// CATCH unit testing headers
-#define CATCH_CONFIG_MAIN  // tell CATCH to provide a main() 
-#include "catch.hpp"
-
-// jsontool headers
-#include "jsontool.h"
-
-// test cases
-#include "variant.h"
